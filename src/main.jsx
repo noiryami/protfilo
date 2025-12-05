5df007fe8bb1f48cd1068a975b4cb75d388d0a46
noiryami <mahmedkamel388@gmail.com>
2025-12-05 20:25:38 +0200

    Add terser dependency for production build

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { SpeedInsights } from '@vercel/speed-insights/react'
-import '@vercel/speed-insights/styles.css'
 import './index.css' 
 import App from './App.jsx'
 
