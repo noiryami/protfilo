88d90f62a3efa72b0d7654bf0c75f07ae2a70db2
noiryami <mahmedkamel388@gmail.com>
2025-12-14 21:26:43 +0200

    last commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import { FaFacebookF } from "react-icons/fa";
+import { FaFacebookF, FaWhatsapp } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import styles from "./styles.module.css";
@@ -18,29 +18,37 @@ const Footer = () => {
         </li>
         <li>
           <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#skills">Skills</a>
-          </li>
-          <li>
-            <a href="#services">Services</a>
-          </li>
-          <li>
-            <a href="#projects">Projects</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
+        </li>
+        <li>
+          <a href="#skills">Skills</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
       </ul>
 
       <div className={footerSocials}>
-        <a href="" target="_blank"><FaFacebookF/></a>
-        <a href="" target="_blank"><FaInstagram/></a>
-        <a href="" target="_blank"><FaXTwitter/></a>
+        <a href="https://www.facebook.com/noiryami" target="_blank">
+          <FaFacebookF />
+        </a>
+        <a
+          href="https://api.whatsapp.com/send?phone=201121016866"
+          target="_blank"
+        >
+          <FaWhatsapp />
+        </a>
       </div>
 
       <div className={footerCopyright}>
-          <small>&copy; <a href="#">Muhammad Kamel</a> All rights reseved</small>
+        <small>
+          &copy; <a href="#">Muhammad Kamel</a> All rights reseved
+        </small>
       </div>
     </footer>
   );
